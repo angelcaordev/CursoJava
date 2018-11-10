031bd074c896a9a6f7d6d2bdb8af9f8fe4355bea
angelcaordev@gmail.com <angel@LAPTOP-RLT8GRF3>
2018-11-10 12:11:05 +0100

    Version de pruebas

---
+++ HolaDeNuevo/src/holaDeNuevo/Principal.java
@@ -0,0 +1,11 @@
+package holaDeNuevo;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola desde GIT");
+		System.out.println("Esta es la segunda modificación del archivo");
+	}
+
+}
